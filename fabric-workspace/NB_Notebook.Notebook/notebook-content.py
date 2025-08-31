90e47639fcc0e3d3cffbef5f3127659a756cc81a
Tom Keim <Tom.Keim@infosupport.com>
2025-08-31 10:39:07 +0000

    Committing 1 item from workspace 4052e5a9-8a36-4838-9592-922c5e2a7dd1

--- fabric-workspace/NB_Notebook.Notebook/notebook-content.py
+++ fabric-workspace/NB_Notebook.Notebook/notebook-content.py
@@ -51,16 +51,12 @@ def get_connected_lakehouse_workspace() -> str:
         raise ValueError("No Lakehouse items found.")
 
     workspace_id = lakehouses.iloc[0]['Workspace Id']
-    print(workspace_id)
-    match workspace_id:
-        case '4052e5a9-8a36-4838-9592-922c5e2a7dd1':
-            return "DEV"
-        case 'e10ae2d3-b7c3-43ad-8cc2-4680c4b17e27':
-            return "TEST"
-        case '04ec29e9-d622-4c11-a868-f680497c5328':
-            return "PROD"
-        case _:
-            return "Unknown"
+    mapping = {
+        '3b' + '7d2652-0466-4156-a849-1e32ec57d563': "DEV",
+        'e10ae2d3-b7c3-43ad-8cc2-4680c4b17e27': "TEST",
+        '04ec29e9-d622-4c11-a868-f680497c5328': "PROD"
+    }
+    return mapping.get(workspace_id, "Unknown")
 
 print(f"Connected lakehouse is connected to the {get_connected_lakehouse_workspace()} environment")
 
