d282e480c041f63a6b36f4c7e72f68f0bcf16f60
Tom Keim <Tom.Keim@infosupport.com>
2025-08-31 19:32:20 +0000

    Committing 1 item from workspace 4052e5a9-8a36-4838-9592-922c5e2a7dd1

--- fabric-workspace/NB_Notebook.Notebook/notebook-content.py
+++ fabric-workspace/NB_Notebook.Notebook/notebook-content.py
@@ -16,49 +16,26 @@
 # META           "id": "3b7d2652-0466-4156-a849-1e32ec57d563"
 # META         }
 # META       ]
+# META     },
+# META     "environment": {
+# META       "environmentId": "7970d0e5-d3ae-95a4-4c2d-a2d1f6ddaafa",
+# META       "workspaceId": "00000000-0000-0000-0000-000000000000"
 # META     }
 # META   }
 # META }
 
-# MARKDOWN ********************
-
-# - 4052e5a9-8a36-4838-9592-922c5e2a7dd1 = Dev
-# - e10ae2d3-b7c3-43ad-8cc2-4680c4b17e27 = Test
-# - 04ec29e9-d622-4c11-a868-f680497c5328 = Prod
-
 # CELL ********************
 
-# Install sempy
-%pip install semantic-link --q 
+import pkgutil
+import importlib
 
-# METADATA ********************
+def list_subpackages(package_name):
+    package = importlib.import_module(package_name)
+    print("Package path:", package.__path__)  # Debug print
+    return [name for _, name, is_pkg in pkgutil.iter_modules(package.__path__) if is_pkg]
 
-# META {
-# META   "language": "python",
-# META   "language_group": "synapse_pyspark"
-# META }
-
-# CELL ********************
-
-# Do an API call to get the attached notebook workspace ID
-import sempy.fabric as fabric
-
-def get_connected_lakehouse_workspace() -> str:
-    df = fabric.list_items()
-    # first Lakehouse row (case-insensitive), then get its Workspace Id
-    lakehouses = df[df['Type'].str.strip().str.lower() == 'lakehouse']
-    if lakehouses.empty:
-        raise ValueError("No Lakehouse items found.")
-
-    workspace_id = lakehouses.iloc[0]['Workspace Id']
-    mapping = {
-        '3b' + '7d2652-0466-4156-a849-1e32ec57d563': "DEV",
-        'e10ae2d3-b7c3-43ad-8cc2-4680c4b17e27': "TEST",
-        '04ec29e9-d622-4c11-a868-f680497c5328': "PROD"
-    }
-    return mapping.get(workspace_id, "Unknown")
-
-print(f"Connected lakehouse is connected to the {get_connected_lakehouse_workspace()} environment")
+# Use the correct package name based on your structure:
+print(list_subpackages("iska_python_project.iska_python_project"))
 
 # METADATA ********************
 
